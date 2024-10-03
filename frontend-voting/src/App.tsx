1d128e48502529b83ec2a788283e481cb3f618ab
Your Name <you@example.com>
2024-10-03 15:47:31 +0100

    fix:page not found issue fixed

--- frontend-voting/src/App.tsx
+++ frontend-voting/src/App.tsx
@@ -1,5 +1,6 @@
+// src/App.tsx
 import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Landing from "./pages/Landing";
 import NavBar from "./components/NavBar";
@@ -8,48 +9,55 @@ import Signup from "./pages/Signup";
 import Registration from "./pages/Registration";
 import Ballotview from "./pages/Ballotview";
 import Home from "./pages/Home";
+import CreateNewPoll from "./pages/CreateNewPoll";
+import PageNotFound from "./components/PageNotFound"; 
+import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-//@ts-ignore
-import CreateNewPoll from "./pages/CreateNewpoll";
-import PageNotFound from "./components/PageNotFound";
+
+const AuthLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+const NonAuthLayout = () => {
+  return (
+    <div className="flex p-1">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   const [authCheck, setAuthCheck] = useState<boolean>(false);
+
   return (
     <div className="overflow-y-auto overflow-x-hidden">
-      <div>
-        <Toaster />
-      </div>
+      <Toaster />
       <div className="h-screen w-screen absolute -z-10 bg-black-p"></div>
-
-      <div className="">
-        {authCheck && (
-          <div className="relative">
-            <NavBar />
-            <Routes>
-              <Route path="*" element={<PageNotFound />} />
+      <div className="relative">
+        <Routes>
+          {!authCheck ? (
+            <Route element={<AuthLayout />}>
               <Route path="/" element={<Landing />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/registration" element={<Registration />} />
               <Route path="/ballotview" element={<Ballotview />} />
-              {/* <Route path="/home" element={<Home />} /> */}
-            </Routes>
-          </div>
-        )}
-
-        {!authCheck && (
-          <div className="p-1 flex">
-            <Sidebar />
-            <Routes>
-              <Route path="*" element={<PageNotFound />} />
               <Route path="/home" element={<Home />} />
               <Route path="/createnewpoll" element={<CreateNewPoll />} />
-            </Routes>
-          </div>
-        )}
-
-        <div></div>
+            </Route>
+          ) : (
+            <Route element={<NonAuthLayout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/createnewpoll" element={<CreateNewPoll />} />
+            </Route>
+          )}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </div>
     </div>
   );
