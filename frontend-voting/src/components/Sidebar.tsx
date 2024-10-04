31a0d694a8a86601acbff703828ad3c8be8bd58b
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-10-04 15:49:08 +0530

    Updated Sidebar component styles: changed width values

--- frontend-voting/src/components/Sidebar.tsx
+++ frontend-voting/src/components/Sidebar.tsx
@@ -6,8 +6,8 @@ export default function Sidebar() {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   return (
-    <div className={`h-[43rem] w-20`}>
-      <div className={`sticky h-[43rem] bg-gray-p ${isCollapsed ? "w-20" : "w-72"} p-4 rounded-xl transition-width duration-300`}>
+    <div className={`h-[43rem] w-70`}>
+      <div className={`sticky h-[43rem] bg-gray-p ${isCollapsed ? "w-20" : "w-60"} p-4 rounded-xl transition-width duration-300`}>
       <div className={`flex flex-col`}>
         <div className={`flex ${isCollapsed ? 'justify-center' : 'justify-between'} items-center`}>
           <h1 className={`text-white text-3xl font-bold text-center mt-2 ${isCollapsed ? "hidden" : "block"}`}>pollQ</h1>
